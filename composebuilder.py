c2bfc4a342078f5d59cc8d295f904aca8e25505c
SimonGodskesen <simon.godskesen92@gmail.com>
2024-03-14 10:04:40 +0000

    cleaned up files

--- composebuilder.py
+++ composebuilder.py
@@ -1,22 +1,24 @@
 import argparse
 
 def generate_docker_compose(num_workers):
-    docker_compose_content = f"""version: "3.3"
+    docker_compose_content = f"""version: "2.4"
 services:"""
 
     for i in range(1, num_workers + 1):
         docker_compose_content += f"""
   worker{i}:
-    image: test:v0
-    command: /bin/bash -c "/usr/local/spark/sbin/start-worker.sh spark://192.168.2.219:7077"
+    image: project:v0
+    command: /bin/bash -c "usr/local/spark/sbin/start-worker.sh spark://192.168.2.62:7077"
     expose:
-      - "8888"
-      - "8080"
+      - "50010"
+      - "9866"
+      - "9867"
       - "7077"
       - "9000"
-    network_mode: "host"
+      - "9001"
     volumes:
       - /home/ubuntu/Drive:/Drive
+    mem_limit: 1600M
 """
 
     with open('docker-compose.yml', 'w') as f:
